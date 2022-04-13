b6ce1f2be3c9dde354aeb5899a3d5575e37a410a
Fabian Amhof <amhof.fabian1234@gmail.com>
2022-04-13 17:59:15 +0200

    added result_5

--- examples/run.py
+++ examples/run.py
@@ -21,7 +21,7 @@ from NeCNN.Method1.genome import NECnnGenome_M1
 from NeCNN.Pytorch.pytorch_converter import create_CNN
 from NeCNN.Pytorch.pytorch_helper import classification_error, train_pytorch
 
-folder = "results_4"
+folder = "results_5"
 
 isExist = os.path.exists(folder)
 
