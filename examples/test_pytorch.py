7fc5c6955e99270cdd8f3d29b0ca1c78709be90b
Fabian Amhof <amhof.fabian1234@gmail.com>
2022-04-20 13:13:34 +0200

    added ability to specify num epochs during training

--- examples/test_pytorch.py
+++ examples/test_pytorch.py
@@ -17,7 +17,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Create the data loaders for the train and test sets
 trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True, num_workers=8)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=8)
 
 testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(trainset, batch_size=4096, shuffle=False, num_workers=2)
@@ -36,9 +36,9 @@ net.classifier = nn.Sequential(*classifier)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.02, momentum=0.5)
-epoch_loss = train_pytorch(net, optimizer, criterion, trainloader, device=device)
+epoch_loss = train_pytorch(net, optimizer, criterion, trainloader, device=device, epochs=3)
 loss = get_loss(net, criterion, trainloader, device=device)
 print(f"Epoch Loss {epoch_loss}; Loss: {loss};")
 print(f"Classification: {classification_error(net, trainloader, device=device)}")
 
-torch.save(net.state_dict(), 'models/model.pth')
+# torch.save(net.state_dict(), 'models/model.pth')
